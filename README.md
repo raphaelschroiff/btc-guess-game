062e9472bb9a2ffb2d90f1e7448d5f68acef67cd
Raphael Schroiff <2109384+raphaelschroiff@users.noreply.github.com>
2026-01-21 20:45:13 +0100

    Scaffold the CDK app

---
+++ README.md
@@ -0,0 +1,14 @@
+# BTC Guess Game
+
+A litte guessing game about the development of the bitcoin price.
+
+Developed for AWS with TypeScript and CDK.
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
---
+++ test/btc-guess-game.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BtcGuessGame from '../lib/btc-guess-game-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/btc-guess-game-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BtcGuessGame.BtcGuessGameStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
