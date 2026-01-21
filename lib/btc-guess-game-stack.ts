340387ccad9108d7650410321f410c74dcda2805
Raphael Schroiff <2109384+raphaelschroiff@users.noreply.github.com>
2026-01-21 21:53:41 +0100

    add CFN distribution and S3 Bucket to host static files

--- lib/btc-guess-game-stack.ts
+++ lib/btc-guess-game-stack.ts
@@ -1,16 +1,45 @@
+import { AllowedMethods, Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { PolicyStatement, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BtcGuessGameStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const appBucket = new Bucket(this, 'BtcGuessGameAppBucket', {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
 
-    // The code that defines your stack goes here
+    const distribution = new Distribution(this, 'BtcGuessGameDistribution', {
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessControl(appBucket),
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
+      },
+      defaultRootObject: 'index.html',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BtcGuessGameQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cfAccesspolicy = new PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [`${appBucket.bucketArn}/*`],
+      principals: [ ServicePrincipal.fromStaticServicePrincipleName('cloudfront.amazonaws.com')],
+      conditions: {
+        'StringEquals': {
+          'AWS:SourceArn': distribution.distributionArn
+        }
+      }
+    });
+
+    appBucket.addToResourcePolicy(cfAccesspolicy);
+
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+    });
+
+    new cdk.CfnOutput(this, 'AppBucketName', {
+      value: appBucket.bucketName,
+    });
   }
 }
