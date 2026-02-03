40c0728d7933d09a9f38c756ce1d31a8e9a06b17
Raphael Schroiff <2109384+raphaelschroiff@users.noreply.github.com>
2026-02-03 15:32:35 +0100

    animate user score on update

--- spa/src/components/user-score/user-score.tsx
+++ spa/src/components/user-score/user-score.tsx
@@ -1,10 +1,13 @@
-import  styles from './user-score.module.css';
+import { useAnimateOnUpdate } from '../../hooks/useAnimateOnUpdate';
+import styles from './user-score.module.css';
 
-export function UserScore({score}: {score: number}) {
+export function UserScore({ score }: { score: number }) {
+
+  const animationStyles = useAnimateOnUpdate(score, 'wiggle', '.3s', 'ease-in-out');
 
   return (
-    <div class={styles.userScore}>
-      Score: {score}
+    <div class={styles.userScore} style={animationStyles}>
+      Your Score: {score}
     </div>
   );
 }
